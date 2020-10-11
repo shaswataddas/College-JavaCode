88d69479af28ea0771ed32fe33279239e0a72dfc
Shaswata Das <shaswata0999@gmail.com>
2020-10-11 21:52:10 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,16 @@
+class prime{
+	public static void main(String[] args) {
+		int number = 5;
+		boolean flag = false;
+		for (int i=2;i<=number/2;i++) {
+			if (number%i==0) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+			System.out.println(number + " is a prime number.");
+		else
+			System.out.println(number + " is not a prime number.");
+	}
+}
