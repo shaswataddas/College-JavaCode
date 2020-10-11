c528e8fd2dc0e7942ce03e359944aaea0369bb1a
Shaswata Das <shaswata0999@gmail.com>
2020-10-11 21:49:32 +0530

    Add files via upload

---
+++ sum_of_two.java
@@ -0,0 +1,10 @@
+class sum_of_two{
+	public static void main(String[] args) {
+		float a = 9;		// First number
+		float b = 20;		// Second number
+		float sum = a+b;	// sum of two number
+		float ave = sum/2;	// Average of these two number
+		System.out.println("The Sum of "+str(a)+" and "+b+" is = "+sum);
+		System.out.print("The Average of "+a+" and "+b+" is = "+ave);
+	}
+}
