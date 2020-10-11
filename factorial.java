ab234127d9d63d3bbe1c9e45820d92f7983036bc
Shaswata Das <shaswata0999@gmail.com>
2020-10-11 21:57:02 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,9 @@
+class factorial{
+	public static void main(String[] args) {
+		int fact=1;
+		int number = 5;
+		for(int i=1;i<=number;i++)
+			fact = fact*i;
+		System.out.println("Factorial of "+number+" is - "+fact);
+	}
+}
